0534b0e5d0d173ddc7e3b7fd759004d9db70135e
Jun Yeu Mah <junyeumah@gmail.com>
2020-05-17 15:20:09 +1000

    first commit

---
+++ src/components/fixedColumnsLayout/Default/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+const App = () => (
+  <>
+    <div class="header">Header</div>
+    <div class="left">Left</div>
+    <div class="center">Center</div>
+    <div class="right">Right</div>
+    <div class="footer">Footer</div>
+  </>
+)
+
+export default App;
---
+++ src/components/responsiveColumnsLayout/Bootstrap4/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.css';
+
+const App = () => (
+  <>
+    <div class="header">Header</div>
+    <div class="blogposts">
+      <div class="blogpost">Blog Post 1</div>
+      <div class="blogpost">Blog Post 2</div>
+      <div class="blogpost">Blog Post 3</div>
+      <div class="blogpost">Blog Post 4</div>
+      <div class="blogpost">Blog Post 5</div>
+      <div class="blogpost">Blog Post 6</div>
+      <div class="blogpost">Blog Post 7</div>
+    </div>
+    <div class="footer">Footer</div>
+  </>
+)
+
+export default App;
