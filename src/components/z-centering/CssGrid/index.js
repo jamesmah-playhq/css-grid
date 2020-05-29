037f05491710135c448a2c63a2448c6f307e1117
James Mah <james.mah@playhq.com>
2020-05-29 13:08:48 +1000

    Add centering

---
+++ src/components/z-centering/CssGrid/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+const App = () => (
+  <div class="container">
+    <div class="header">Header</div>
+    <div class="row">
+      <div class="blogpost"><div>Blog Post 1</div></div>
+      <div class="blogpost">Blog Post 2</div>
+      <div class="blogpost">Blog Post 3</div>
+    </div>
+    <div class="footer">Footer</div>
+  </div>
+)
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './components/fixedColumnsLayout/Bootstrap3';
+import App from './components/fixedColumnsLayout/Bootstrap3';
 // import App from './components/fixedColumnsLayout/Bootstrap4';
 // import App from './components/fixedColumnsLayout/CssGrid';
 // import App from './components/fixedColumnsLayout/CssGridAreas';
 // import App from './components/responsiveColumnsLayout/Bootstrap4';
 // import App from './components/responsiveColumnsLayout/CssGrid';
-import App from './components/responsiveColumnsLayout/CssGridAutoFlow';
+// import App from './components/responsiveColumnsLayout/CssGridAutoFlow';
+// import App from './components/z-centering/CssGrid';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
